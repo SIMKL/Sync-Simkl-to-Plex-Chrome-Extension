1fc8d1e320eb49355ad11be3fa4a29092d205000
phanirithvij <phanirithvij2000@gmail.com>
2022-03-02 22:31:16 +0530

    inlcude clientid and client secrets

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,17 +12,26 @@ sed -i "s/DEVELOPMENT=true/DEVELOPMENT=false/g" dist/js/common.js
 sed -i -r "s/const DEVELOPMENT_FETCH_REQS=(true|false);//g" dist/js/common.js
 sed -i -r "s/const devLoggerSetup=\(.*\};/const devloggerSetup=_=>_=>\{\}\;/g" dist/js/common.js
 
-# validate generated js files for syntax
 set +ex
+
+if [ ! -f dist/js/background/env.js ]; then
+cat << EOM > dist/js/background/env.js
+const SimklClientID = "$SIMKL_CLIENT_ID";
+const SimklClientSecret = "$SIMKL_CLIENT_SECRET}}";
+EOM
+fi
+
 for i in $(find dist -type f -name "*.js"); do
     # removing consoledebug calls
     sed -i -r "s/consoledebug\([^;]*\)\(\);/;/g" "$i"
     sed -i -r "s/console.debug\([^;]*\);/;/g" "$i"
     minify -o $i $i
+    # validate generated js files for syntax
     echo + "node -c $i"
     env NO_COLOR=1 node -c $i
     if [ $? -ne 0 ]; then
         exit 1 # fail fast
     fi
 done
+
 set -ex
